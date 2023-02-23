c79f52a6d826881f3c1ac55b95db7614a5441291
Tshego <cavinrakoma@gmail.com>
2023-02-23 08:08:17 -0800

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H
+
 #define MAIN_H
 
 #include <stdio.h>
@@ -9,5 +10,5 @@ int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 void print_times_table(int n);
 
-#endif 
+#endif /* MAIN_H */i 
 
