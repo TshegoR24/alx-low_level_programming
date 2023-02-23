a08eada5ece5d72971528952bc15bf16fc21848f
Tshego <cavinrakoma@gmail.com>
2023-02-23 05:24:35 -0800

    1 Task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,12 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
-
 i = 0;
-/*
-*while (i < 10)
-*{
-*                putchar(i);
-*}
-prinf("Infinite loop avoided! \\0/\n");
+/*while (i < 10)*/
+/*{*/
+/*       putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\0/\n");
 
 return (0);
 }
